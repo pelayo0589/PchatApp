20737b6caa87dc56cbc77d472ac6afb9553895fb
Miguel Pelayo <miguelpelayo@Miguels-MacBook-Pro.local>
2018-06-29 15:56:49 -0400

    Lesson 3

---
+++ PchatApps/View/GradientView.swift
@@ -0,0 +1,42 @@
+//
+//  GradientView.swift
+//  PchatApps
+//
+//  Created by Miguel Pelayo on 6/29/18.
+//  Copyright © 2018 Miguel Pelayo. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+   
+    @IBInspectable var bottonColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        
+        gradientLayer.colors = [topColor.cgColor ,bottonColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        
+        
+        
+    }
+
+}
